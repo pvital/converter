a2662699100fe8f3a9058ad8afc42040a882bd87
Paulo Vital <pvital@gmail.com>
2015-05-23 19:12:18 -0300

    Add src directory and main.c file
    Signed-off-by: Paulo Vital <pvital@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  converter - Command-line number converter to Mac OS
+//
+//  Created by Paulo Ricardo Paz Vital on 23/05/15.
+//  Copyright (c) 2015 pvital Solutions. All rights reserved.
+//
+
+#include <stdio.h>
+
+void usage(void) {
+    printf("usage: convert decimal_number \n");
+}
+
+int main(int argc, const char * argv[]) {
+    usage();
+    return 0;
+}
