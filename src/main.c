ce37c4c11c5fb76788b2b6a04c51107941b6f518
Paulo Vital <pvital@gmail.com>
2015-05-23 21:43:25 -0300

    Add decimal to binary conversion.
    Add function to convert decimal to binary.
    
    Signed-off-by: Paulo Vital <pvital@gmail.com>

--- src/main.c
+++ src/main.c
@@ -14,6 +14,28 @@ void usage(void) {
     printf("usage: convert decimal_number \n");
 }
 
+void dec2bin(int decimal) {
+    int remainder[32];
+    int quocient = decimal, i = 0;
+    
+    while (quocient >= 2) {
+        remainder[i] = quocient % 2;
+        quocient = quocient / 2;
+        i++;
+    }
+    
+    // add the last quocient in the end of remainder list
+    remainder[i] = quocient;
+    
+    // print the remainder list in the revert order
+    printf ("The decimal number %d in binary is: ", decimal);
+    while (i >= 0) {
+        printf("%d", remainder[i]);
+        i--;
+    }
+    printf("\n");
+}
+
 int main(int argc, const char * argv[]) {
     int decimal;
     
@@ -32,6 +54,7 @@ int main(int argc, const char * argv[]) {
         printf("ERROR: maximum decimal number supported is %d.\n", INT_MAX);
         return -1;
     }
-    
+
+    dec2bin(decimal);
     return 0;
 }
