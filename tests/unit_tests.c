ccd5a7b4be39a3e3d0bae2e779e2590abb6c94c3
Paulo Vital <pvital@gmail.com>
2015-06-22 19:28:09 -0300

    Added support to unit tests.
    Initial implementation of unit tests based on current code.
    
    Signed-off-by: Paulo Vital <pvital@gmail.com>

---
+++ tests/unit_tests.c
@@ -0,0 +1,21 @@
+//
+//  unit_tests.c
+//  converter
+//
+//  Created by Paulo Ricardo Paz Vital on 22/06/15.
+//  Copyright (c) 2015 pvital Solutions. All rights reserved.
+//
+
+#include <stdio.h>
+#include "binary.h"
+#include "octal.h"
+#include "hexa.h"
+
+int main (int argc, const char * argv[]) {
+    int decimal = 12785;
+    
+    dec2bin(decimal);
+    dec2oct(decimal);
+    dec2hexa(decimal);
+
+}
